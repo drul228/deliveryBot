9db3af3f101f38d027f8ea2312e033db5ff1a196
drul228 <t_kostyukevich@mail.ru>
2023-05-11 15:59:16 +0300

    Remarks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,43 @@
-package org.example;
-
 import java.util.*;
 
 public class Main {
-    public static final int ROADS = 1000;
+    final static String LETTERS = "RLRFR";
+    final static int ROUTE_LENGTH = 100;
+    final static int AMOUNT_OF_THREADS = 1000;
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
-    public static int countOfR = 0;
-    public static final int howMuchRis = 0;
 
+    public static void main(String[] args) {
+
+        for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
+            new Thread(() -> {
+                String route = generateRoute(LETTERS, ROUTE_LENGTH);
+                int frequency = (int) route.chars().filter(c -> c == 'R').count();
+                synchronized (sizeToFreq) {
+                    if (sizeToFreq.containsKey(frequency)) {
+                        sizeToFreq.put(frequency, sizeToFreq.get(frequency) + 1);
+                    } else {
+                        sizeToFreq.put(frequency, 1);
+                    }
+                }
+            }).start();
+        }
+
+        Map.Entry<Integer, Integer> max = sizeToFreq
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get();
+        System.out.println("Самое частое количество повторений " + max.getKey()
+                + " (встретилось " + max.getValue() + " раз)");
+
+
+        System.out.println("Другие размеры: ");
+        sizeToFreq
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(e -> System.out.println(" - " + e.getKey() + " (" + e.getValue() + " раз)"));
+    }
 
     public static String generateRoute(String letters, int length) {
         Random random = new Random();
@@ -16,51 +46,5 @@ public class Main {
             route.append(letters.charAt(random.nextInt(letters.length())));
         }
         return route.toString();
-    }
-
-    public static int repeatR(String text) {
-        int maxSize = 0;
-        for (int i = 0; i < text.length(); i++) {
-            for (int j = 0; j < text.length(); j++) {
-                if (i >= j) {
-                    continue;
-                }
-                boolean bFound = false;
-                for (int k = i; k < j; k++) {
-                    if (text.charAt(k) != 'R') {
-                        bFound = true;
-                        break;
-                    }
-                }
-                if (!bFound && maxSize < j - i) {
-                    maxSize = j - i;
-                }
-            }
-        }
-        return maxSize;
-    }
-
-    public static List<Thread> intialisationThreads(int count) {
-        List<Thread> threadList = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            threadList.add(new Thread(() -> {
-                synchronized (sizeToFreq) {
-                    var text = generateRoute("RLRFR", 100);
-                    var repeatR = repeatR(text);
-                    if (sizeToFreq.containsKey(repeatR)) {
-                        var countOfR = sizeToFreq.get(repeatR);
-                        sizeToFreq.put(repeatR,countOfR + 1);
-                    }
-                    sizeToFreq.putIfAbsent(repeatR,1);
-                }
-            }));
-        }
-        return threadList;
-    }
-
-
-    public static void main(String[] args) {
-        intialisationThreads(ROADS).forEach(thread -> thread.start());
-        sizeToFreq.entrySet().stream().sorted(((o1, o2) -> o2.getValue().compareTo(o1.getValue()))).forEach(System.out::println);
     }
 }
